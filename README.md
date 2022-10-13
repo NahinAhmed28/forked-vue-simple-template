6cfc47c8ae2d0d656fc2b959dfa1cb7d7e9271e1
NahinAhmed28 <72101670+NahinAhmed28@users.noreply.github.com>
2022-10-14 03:12:52 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Vue 3 with Bootstrap 5
+
+## About this repository
+This repository provides a simple authentication UI build on Vue 3. You can fork this repository for your frontend application and olso if you are familiar with Laravel, you can even use backend repository on link below.
+
+- Laravel API's [repository](https://github.com/lindritkrasniqi/sanctum-auth-template).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/routes/index.js
@@ -0,0 +1,89 @@
+/* eslint-disable no-unused-vars */
+import { createRouter, createWebHistory } from "vue-router";
+import store from "@/store";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/pages/index"),
+      name: "index",
+      meta: {
+        layout: "loggedIn",
+        requiresAuth: false,
+      },
+    },
+
+    {
+      path: "/accounts/login",
+      component: () => import("@/pages/accounts/login"),
+      name: "accounts-login",
+      beforeEnter: (to, from, next) => {
+        if (store.state.auth.loggedIn) next({ name: "index" });
+        else next();
+      },
+    },
+
+    {
+      path: "/accounts/logout",
+      component: () => import("@/pages/accounts/logout"),
+      name: "accounts-logout",
+      meta: {
+        requiresAuth: true,
+      },
+    },
+
+    {
+      path: "/accounts/reset",
+      component: () => import("@/pages/accounts/reset"),
+      name: "accounts-reset",
+      beforeEnter: (to, from, next) => {
+        if (store.state.auth.loggedIn || !Object.keys(to.query).length) next({ name: "index" });
+        else next();
+      },
+    },
+
+    {
+      path: "/accounts/forgot",
+      component: () => import("@/pages/accounts/forgot"),
+      name: "accounts-forgot",
+      beforeEnter: (to, from, next) => {
+        if (store.state.auth.loggedIn) next({ name: "index" });
+        else next();
+      },
+    },
+
+    {
+      path: "/accounts/signup",
+      component: () => import("@/pages/accounts/signup"),
+      name: "accounts-signup",
+      beforeEnter: (to, from, next) => {
+        if (store.state.auth.loggedIn) next({ name: "index" });
+        else next();
+      },
+    },
+
+    {
+      path: "/*",
+      component: () => import("@/layouts/error"),
+      name: "error",
+      meta: {
+        layout: "error",
+      },
+    },
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0 };
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.auth.loggedIn) {
+    next({ name: "accounts-login" });
+  } else {
+    next();
+  }
+});
+
+export default router;
